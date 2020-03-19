a222585ddcd8c5b901a326243da5be1b962337f8
jacheampong <jacheampong@gmail.com>
2020-03-18 21:23:45 -0400

    #8 Update Spring Security

--- src/main/java/com/ja/dev/pma/security/SecurityConfiguration.java
+++ src/main/java/com/ja/dev/pma/security/SecurityConfiguration.java
@@ -4,14 +4,12 @@ package com.ja.dev.pma.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,32 +18,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource dataSource;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
 	
-	/**
-	 * authentication - who 
-	 * using in memory authentication 
-	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-		.withDefaultSchema()
-			.withUser("myuser")
-				.password("pass1")
-				.roles("USER")
-			.and()
-			.withUser("jon")
-				.password("pass2")
-				.roles("USER")
-			.and()
-			.withUser("manager")
-				.password("pass3")
-				.roles("ADMIN");
-				
-	}
-	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		auth.jdbcAuthentication()
+		.usersByUsernameQuery("select username, password, enabled "
+				+ "from user_accounts where username = ?")
+		.authoritiesByUsernameQuery("select username, role from "
+				+ "user_accounts where username = ?")
+		.dataSource(dataSource)
+		.passwordEncoder(bCryptEncoder);
 	}
 	
 	/**
@@ -58,13 +42,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/project/new").hasRole("ADMIN")
+			.antMatchers("/project/save").hasRole("ADMIN")
 			.antMatchers("/employee/new").hasRole("ADMIN")
-			.antMatchers("/h2_console/**").permitAll()
-			.antMatchers("/").authenticated().and().formLogin();
-		
-		// disabled in order to be able to access H2 DB url
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
+			.antMatchers("/employee/save").hasRole("ADMIN")
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
+//		.formLogin().loginPage("login-page"); custom login page
+
 	}
 
 }
